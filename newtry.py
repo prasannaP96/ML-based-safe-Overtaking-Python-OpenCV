e790bc4a324b2af3117d562fb0fb8d1a42bacd42
prasannaP96 <59040809+prasannaP96@users.noreply.github.com>
2020-11-11 18:57:02 +0530

    Update newtry.py

--- newtry.py
+++ newtry.py
@@ -3,24 +3,31 @@ import os
 import imutils
 import cv2
 import glob
-#import pandas as pd
-#import xlrd
+import xlrd
 import time
-#import smtplib
-import socket
+import smtplib
+from tkinter import *
+import tkinter as tk
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
-#from selenium import webdriver
-from twilio.rest import Client
 
-list_1 = []
-list_2 = []
-list_3 = []
 C=0
 D=0
 P=0
 Q=0
 S=1
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "Hello, World!"
+@app.route("/sms", methods=['POST'])
+def sms_reply():
+    msg = request.form.get('Body')
+    resp = MessagingResponse()
+    resp.message("U can Overtake")
+    return str(resp)
+
+
 def find_marker(image):
     # convert the image to grayscale, blur it, and detect edges
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -45,16 +52,14 @@ def find_marker_1(image_1):
     d = max(conts, key=cv2.contourArea)
     # compute the bounding box of the of the paper region and return it
     return cv2.minAreaRect(d)
+
 def distance_to_camera(knownWidth, focalLength, perWidth):
     # compute and return the distance from the maker to the camera
     return (knownWidth * focalLength) / perWidth
 
+
 KNOWN_DISTANCE = [12,6]
 KNOWN_WIDTH = [8.5,0.5]
-#real_Height = 11.6929
-#image_Height = 720
-#object_Height = 1122.519685
-#sensor_Height = 0.1968
 time_diff = 1
 low_white = np.array([0,0,0])
 high_white = np.array([0,0,255])
@@ -69,47 +74,26 @@ focalLength = (marker[1][0] * KNOWN_DISTANCE[0]) / KNOWN_WIDTH[0]
 focalLength_1 = (marker_1[1][0] * KNOWN_DISTANCE[1]) / KNOWN_WIDTH[1]
 video = cv2.VideoCapture(0)
 count = 1
-#server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-#ip = socket.gethostbyname(socket.gethostname())
-#port = 1234
-#address = ((ip,port))
-#server.bind(address)
-#server.listen(0)
-#print("socket is listening")
-#client, addr = server.accept()
-#print("connected")
-account_sid = 'AC19f9340f70eb57a4d02ff9b27fc3bc4d'
-auth_token = '***************'
-client = Client(account_sid, auth_token)
-driver = webdriver.Chrome("C:\webdrivers\chromedriver")
-driver.get('http://web.whatsapp.com')
-name = input('Enter the name of user or group : ')
-user = driver.find_element_by_xpath("//span[@title='Twilio']".format(name))
-user.click()
-msg_box = driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[2]/div/div[2]")
-msg_box.send_keys("Shall i Overtake now?")
-driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[3]/button").click()
 
 while True:
-
-
     t = time.time()
     check, frame = video.read()
-    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    #hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     marker = find_marker(frame)
-    marker_1 =find_marker_1(frame)
-    mask = cv2.inRange(hsv_frame, low_white, high_white)
-    mask_1 = cv2.inRange(hsv_frame, low_gray, high_gray)
-    cnts = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
-    cnts_1 = cv2.findContours(mask_1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cnts_1 = imutils.grab_contours(cnts_1)
-    inches = distance_to_camera(KNOWN_WIDTH[0], focalLength, marker[1][0])
+    marker_1 = find_marker_1(frame)
+    #mask = cv2.inRange(hsv_frame, low_white, high_white)
+    #mask_1 = cv2.inRange(hsv_frame, low_gray, high_gray)
+    #cnts = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    #cnts = imutils.grab_contours(cnts)
+    #cnts_1 = cv2.findContours(mask_1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    #cnts_1 = imutils.grab_contours(cnts_1)
+    inches = distance_to_camera(KNOWN_WIDTH[0], focalLength, marker[1][0])    
     inches_1 = distance_to_camera(KNOWN_WIDTH[1], focalLength_1, marker_1[1][0])
     box = cv2.cv.BoxPoints(marker) if imutils.is_cv2() else cv2.boxPoints(marker)
     box = np.int0(box)
     box_1 = cv2.cv.BoxPoints(marker_1) if imutils.is_cv2() else cv2.boxPoints(marker_1)
     box_1 = np.int0(box_1)
+    
     range = cv2.contourArea(box)
     range_1 = cv2.contourArea(box_1)
     if(17500<range<100900):
@@ -119,73 +103,40 @@ while True:
 
         F = round((inches/12),2)
         #print(time.time()-t)
-        list_1.append(F)
         #print("Distance_1", F)
         G = (F-C)/((time_diff+(time.time()-t))*S)# Velocity
         #print("Vel_1", G)
-        list_2.append(G)
+        
         H = F-C
         #print("Acc",(H-D)/((time_diff))
         J = (H-D)/(((time_diff +(time.time()-t))**2)*S) # Acceleration
         #print("Acc_1", J)
-        list_3.append(J)
+        
         #t = time.time()
         D = F-C
         C = (inches / 12)
-        for c in cnts:
-            area = cv2.contourArea(c)
+        #for c in cnts:
+            #area = cv2.contourArea(c)
             #print(area)
-            if (area > 5):
-                pass
+            #if (area > 5):
+                #pass
                 #print("white")
-                #list_4.append("white")
+                
         if (F>=40):
-            #print("Condition satisfied for Distance")
-            #print("Now Checking for Velocity")
+            print("Condition satisfied for Distance")
+            print("Now Checking for Velocity")
             if (G>=17):
-                #print("Checking for Acceleration")
+                print("Checking for Acceleration")
                 if (J<0):
                     print("Dont Over take")
-                    message = client.messages \
-                        .create(
-                        body="Don't Overtake",
-                        from_='whatsapp:+14155238886',
-                        to='whatsapp:+919944334767'
-                    )
-                    #client.send(bytes("Dont Overtake Now!", "utf-8"))
                 elif (J>=3.5):
-                    print('Accelerate to Overtake')
-                    #c.send(bytes('Accelerate to Overtake', 'utf-8'))
-                    #client.send(bytes("Accelerate to overtake", "utf-8"))
+                    print('We need to accelerate to Overtake')
                 elif (J>0):
-                    message = client.messages \
-                        .create(
-                        body="Overtake Now",
-                        from_='whatsapp:+14155238886',
-                        to='whatsapp:+919944334767'
-                    )
                     print("normally overtake")
-                    #c.send(bytes('Overtake Now', 'utf-8'))
-                    #client.send(bytes("Overtake Now", "utf-8"))
             else:
-                message = client.messages \
-                    .create(
-                    body="Don't Overtake",
-                    from_='whatsapp:+14155238886',
-                    to='whatsapp:+919944334767'
-                )
                 print("Do Not Overtake")
-                #c.send(bytes("Don't Overtake Now", 'utf-8'))
-                #client.send(bytes("Dont' Overtake Now", "utf-8"))
         else:
-            #print("Dont over take")
-            message = client.messages \
-                .create(
-                body="Don't Overtake",
-                from_='whatsapp:+14155238886',
-                to='whatsapp:+919944334767'
-            )
-            #client.send(bytes(" Don't Overtake Now", "utf-8"))
+            print("Dont over take")
         cv2.putText(frame, "Dis=%.2fft" % (inches / 12), (frame.shape[1] - 240, frame.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
                     1, (0, 255, 0), 2)
         cv2.putText(frame, "Vel=%.2fft\sec" %G, (frame.shape[1] - 240, frame.shape[0] - 80),
@@ -195,10 +146,11 @@ while True:
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255,0),2)
         count = 1
     elif (1600<range_1<5500):
-        for c in cnts_1:
-            area_1 = cv2.contourArea(c)
-            if (area_1 > 100000):
-                print("Black")
+        #for c in cnts_1:
+            #area_1 = cv2.contourArea(c)
+            #if (area_1 > 100000):
+                #pass
+                #print("Black")
                 #list_8.append(print("Black"))
         cv2.drawContours(frame, [box_1], -1, (255, 0, 0), 2)
 
@@ -206,71 +158,34 @@ while True:
         # print("Distance",F)
         # print("Vel",(F-C)/time_diff)
         #print(time.time()-t)
+        
         #print("Dist_2",F_1)
         G_1 = (F_1 -P) / ((time_diff + (time.time() - t)) * S)  # Velocity
+        #list_6.append(G_1)
         #print("Vel_2",G_1)
         H_1 = F_1 - P
         # print("Acc",(H-D)/((time_diff)))
-        J_1 = (H_1 - Q) / (((time_diff + (time.time() - t)) ** 2) * S)  # Accelerati
+        J_1 = (H_1 - Q) / (((time_diff + (time.time() - t)) ** 2) * S)  # Acceleration
+        #list_7.append(J_1)
         #print("Acc_2",J_1)
         t = time.time()
         Q = F_1 -P
         P = (inches_1 / 12)
         if (F_1 >= 40):
-            #print("Condition satisfied for Distance")
-            #print("Now Checking for Velocity")
+            print("Condition satisfied for Distance")
+            print("Now Checking for Velocity")
             if (G_1 >= 17):
-                #print("Checking for Acceleration")
+                print("Checking for Acceleration")
                 if (J_1 < 0):
-                    message = client.messages \
-                        .create(
-                        body="Don't Overtake",
-                        from_='whatsapp:+14155238886',
-                        to='whatsapp:+919944334767'
-                    )
                     print("Dont Over take")
-                    #c.send(bytes("Don't Overtake ", 'utf-8'))
-                    #client.send(bytes("Don't Overtake Now", "utf-8"))
                 elif (J_1 >= 3.5):
-                    message = client.messages \
-                        .create(
-                        body="Accelerate to Overtake",
-                        from_='whatsapp:+14155238886',
-                        to='whatsapp:+919944334767'
-                    )
                     print('We need to accelerate to Overtake')
-                    #c.send(bytes("Accelerate to overtake", 'utf-8'))
-                    #client.send(bytes("Accelerate to Overtake Now", "utf-8"))
                 elif (J_1 > 0):
-                    message = client.messages \
-                        .create(
-                        body="Overtake Now",
-                        from_='whatsapp:+14155238886',
-                        to='whatsapp:+919944334767'
-                    )
                     print("normally overtake")
-                    #c.send(bytes("Overtake Now", 'utf-8'))
-                    #client.send(bytes("Overtake Now", "utf-8"))
             else:
-                #print("Do Not Overtake")
-                message = client.messages \
-                    .create(
-                    body="Don't Overtake",
-                    from_='whatsapp:+14155238886',
-                    to='whatsapp:+919944334767'
-                )
-                #c.send(bytes("Don't Overtake ", 'utf-8'))
-                #client.send(bytes("Don't Overtake Now", "utf-8"))
+                print("Do Not Overtake")
         else:
-            message = client.messages \
-                .create(
-                body="Don't Overtake",
-                from_='whatsapp:+14155238886',
-                to='whatsapp:+919944334767'
-            )
             print("Dont over take")
-            #c.send(bytes("Don't Overtake Now", 'utf-8'))
-            #client.send(bytes("Don't Overtake Now", "utf-8"))
         cv2.putText(frame, "Dis=%.2fft" % (inches_1 / 12), (frame.shape[1] - 230, frame.shape[0] - 140),
                     cv2.FONT_HERSHEY_SIMPLEX,
                     1, (255, 0, 0), 2)
@@ -281,14 +196,38 @@ while True:
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
         count = 1
     cv2.imshow("image",frame)
+    #smtpUser = 'prasannakumarp96@gmail.com'
+    #smtpPass = '************'
+    #toAdd = 'prasannakumar.p2019@vitstudent.ac.in'
+    #fromAdd = smtpUser
+    #subject = 'Ericsson Test'
+    #header = 'To:' + toAdd + '\n' + 'From:' + fromAdd + '\n' + 'Subject:' + subject
+    if ((20000<range<100900)and(F>=40)and (G>=17)and(J>0)):
+        app.run()
+    elif ((1600<range_1<5500)and(F_1>=40)and(G_1>=17)and(J_1>0)):
+        app.run()
+    else:
+        app.run()
+    #s = smtplib.SMTP('smtp.gmail.com', 587)
+    #s.ehlo()
+    #s.starttls()
+    #s.ehlo()
+    #s.login(smtpUser, smtpPass)
+    #s.sendmail(fromAdd, toAdd, header + '\n' + body)
+    #s.quit()
+    #app.run(debug=True)
     key = cv2.waitKey(1000)
     print()
-
     if key == 27:
         break
 #p = pd.DataFrame()
 #p['Distance'] = list_1
 #p['Velocity'] = list_2
 #p['Acceleration'] = list_3
+#p['Car_1_Color'] = list_4
+#p['Distance1'] = list_5
+#p['Velocity1'] = list_6
+#p['Acceleration1'] = list_7
+#p['Car_2_Color'] = list_8
 #p.to_csv('Data1.csv')
 cv2.destroyAllWindows()
