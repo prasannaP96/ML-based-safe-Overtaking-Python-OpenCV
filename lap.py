712a76813e3d89cc59b611870f8197b758b87835
prasannaP96 <59040809+prasannaP96@users.noreply.github.com>
2020-12-24 09:59:27 +0530

    Update lap.py

--- lap.py
+++ lap.py
@@ -1,19 +1,25 @@
 import socket
 def mob():
-    host = "192.168.1.7"
+    host = "192.168.1.6"
     port = 12345
-    s = socket.socket()
-    s.connect((host, port))
-    print(s.recv(1024).decode())
-    s.close()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    s.listen(5)
+    print('listening')
+    c, addr = s.accept()
+    print(c.recv(1024).decode())
+    c.close()
 def rpi():
-    host = '192.168.1.4'
+    host = "192.168.1.6"
     port = 12346
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((host, port))
     s.listen(5)
+    print('listening')
     c, addr = s.accept()
-    c.send(bytes('shall i overtake now', 'utf-8'))
+    print(c.recv(1024).decode())
     c.close()
 mob()
 rpi()
+
+
