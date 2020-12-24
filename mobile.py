b0011c4daf830640b9e7e80ab849fa292271c970
prasannaP96 <59040809+prasannaP96@users.noreply.github.com>
2020-12-24 10:10:17 +0530

    Update mobile.py

--- mobile.py
+++ mobile.py
@@ -1,22 +1,25 @@
 import socket
-def mob():
-    host = '192.168.1.7'
+def lap():
+    host = '192.168.1.6'
     port = 12345
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((host, port))
-    s.listen(5)
-    c, addr = s.accept()
-    c.send(bytes('shall i overtake now', 'utf-8'))
-    c.close()
+    s = socket.socket()
+    s.connect((host, port))
+    message = input("")
+    s.send(bytes(message, 'utf-8'))
+    s.close()
 def rpi():
-    host = "192.168.1.7"
+    host = "192.168.1.8"
     port = 12347
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((host, port))
     s.listen(5)
     c, addr = s.accept()
-    while True:
+    while True:   #talk(c.recv(1024).decode()) 
+        filename = str("Ericsson.jpeg")
+        file = open(filename,'wb')
+        file_data = c.recv(115200)
+        print("picture received")
+        file.write(file_data)
         print(c.recv(1024).decode())
-    c.close()
-mob()
+lap()
 rpi()
