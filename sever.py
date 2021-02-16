0fcf27474118a794e828c726c9d3e573f25f43ca
prasannaP96 <59040809+prasannaP96@users.noreply.github.com>
2021-02-16 09:52:52 +0530

    Create sever.py

---
+++ sever.py
@@ -0,0 +1,23 @@
+import socket
+def mob():
+    host = "192.168.1.6"
+    port = 12345
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    s.listen(5)
+    print('listening')
+    c, addr = s.accept()
+    print(c.recv(1024).decode())
+    c.close()
+def rpi():
+    host = "192.168.1.6"
+    port = 12346
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    s.listen(5)
+    print('listening')
+    c, addr = s.accept()
+    print(c.recv(1024).decode())
+    c.close()
+mob()
+rpi()
